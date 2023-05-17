6531c5a629bc940e3c882d0c4c1b51d93936ade0
condroagung <63888621+condroagung@users.noreply.github.com>
2023-05-17 10:54:46 +0700

    [FEATURE] add random dice number function

--- app/src/main/java/com/rakamin/diceroller/MainActivity.kt
+++ app/src/main/java/com/rakamin/diceroller/MainActivity.kt
@@ -2,10 +2,27 @@ package com.rakamin.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnRoll = findViewById<Button>(R.id.button)
+        var numberRoll = findViewById<TextView>(R.id.number)
+
+        btnRoll.setOnClickListener {
+            val num = rollNumber()
+            numberRoll.text = num.toString()
+        }
+    }
+
+    fun rollNumber() : Int{
+        val ranNum = (1..6).random()
+        return ranNum
     }
 }
